7a62bad218304d9746a1b89050ee85406014a67d
benoitStH <95411545+benoitStH@users.noreply.github.com>
2022-11-08 16:12:09 +0100

    Update DenavitParameter.cpp

--- DenavitParameter.cpp
+++ DenavitParameter.cpp
@@ -20,20 +20,37 @@ DenavitParameter::DenavitParameter(VariableArticulaire** VARListe)
 
 DenavitParameter::DenavitParameter(DenavitParameter& DENObject)
 {
+	variableArticulaire = (VariableArticulaire**)malloc(sizeof(VariableArticulaire*) * 4);
+	for (int i = 0; i < 4; i++)
+	{
+		variableArticulaire[i] = DENObject.variableArticulaire[i];
+	}
+
 }
 
 DenavitParameter::~DenavitParameter()
 {
+	if (variableArticulaire != nullptr)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			variableArticulaire[i] = nullptr;
+		}
+		free(variableArticulaire);
+	}
 }
 
 void DenavitParameter::AddParameter(int iIndice, VariableArticulaire VARvariable)
 {
+
 }
 
 void DenavitParameter::DeleteParameter(int iIndice)
 {
+
 }
 
 void DenavitParameter::Swap(int iFrom, int iDestination)
 {
+
 }
