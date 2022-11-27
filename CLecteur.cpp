cc8f7718e8f6a65d55ce3f86e9c24316f497b12a
benoitStH <95411545+benoitStH@users.noreply.github.com>
2022-11-27 18:41:25 +0100

    Ajout DenavitParameter [Non Testé]
    Algo de création d'objet DenavitParameter inclus dans la méthode LireFichier()

--- CLecteur.cpp
+++ CLecteur.cpp
@@ -9,14 +9,16 @@ CLecteur::CLecteur(char* pcCheminFichier) : CLecteurBase(pcCheminFichier)
 {
 	pVARListe = nullptr;
 	ppVARVariables = nullptr;
-	uiNbVariables = uiNbElements = 0;
+	pDENParametres = nullptr;
+	uiNbVariables = uiNbElements = uiNbParametres = 0;
 }
 
 CLecteur::CLecteur()
 {
 	pVARListe = nullptr;
 	ppVARVariables = nullptr;
-	uiNbVariables = uiNbElements = 0;
+	pDENParametres = nullptr;
+	uiNbVariables = uiNbElements = uiNbParametres = 0;
 }
 
 CLecteur::~CLecteur()
@@ -38,6 +40,15 @@ CLecteur::~CLecteur()
 			pVARListe[uiBoucle].~Destroy();
 		}
 		free(pVARListe);
+	}
+	
+	if(pDENParametres != nullptr)
+	{
+		for(uiBoucle = 0; uiBoucle < uiNbParametres; uiBoucle++)
+		{
+			pDENParametres[uiBoucle].~Destroy();
+		}
+		free(pDENParametres);
 	}
 }
 
@@ -79,7 +90,7 @@ void CLecteur::LireFichier()
 {
 	Load(); // Ouverture du fichier (sera fermé lorsque le destructeur sera appelé)
 	
-	// Lecture du nombre de variable
+	unsigned int uiBoucle;
 	int iNbElements;
 	unsigned int uiElement = 0; // Elements créés
 	unsigned int uiVariables = 0; // Eléments variables détéctés
@@ -87,6 +98,7 @@ void CLecteur::LireFichier()
 	char* pcTexte;
 	bool bSuccess;
 	
+	// Lecture du nombre de variable
 	pcTexte = FindIntInLine('\0');
 	bSuccess = ToInt(pcTexte, iNbElements);
 	free(pcTexte);
@@ -107,76 +119,116 @@ void CLecteur::LireFichier()
 	
 	NextLine();
 	
-	// Lecture des variables
-	while(EmptyLine() == false)
+	while(EmptyLine() == false) // Tant que la ligne n'est pas vide
 	{
-		
-		// Recherche d'un réel
-		pcTexte = FindDoubleInLine(',');
-		bSuccess = ToDouble(pcTexte, dValeur);
-		free(pcTexte);
-		
-		// S'il n'y a pas pu avoir de conversion
-		if(bSuccess == false)
+		// Lecture des variables jusqu'à la fin de la ligne
+		while(EmptyLine() == false)
 		{
-			RewindTo(',', 1+(EmptyLine() ? 0 : 1), true); // On revient en arrière d'un ou deux cran si suite vide ou non
-			bSuccess = FindWordInFileLine("X", '[');  // S'agit-il d'une valeur variable ?
-			
-			if(bSuccess)
+
+			// Recherche d'un réel
+			pcTexte = FindDoubleInLine(',');
+			bSuccess = ToDouble(pcTexte, dValeur);
+			free(pcTexte);
+
+			// S'il n'y a pas pu avoir de conversion
+			if(bSuccess == false)
 			{
-				// Recherche de la valeur minimale de l'interval
-				pcTexte = FindDoubleInLine(',');
-				bSuccess = ToDouble(pcTexte, dValeur);
-				free(pcTexte);
-				
+				RewindTo(',', 1+(EmptyLine() ? 0 : 1), true); // On revient en arrière d'un ou deux cran si suite vide ou non
+				bSuccess = FindWordInFileLine("X", '[');  // S'agit-il d'une valeur variable ?
+
 				if(bSuccess)
 				{
-					pVARListe[uiElement].ModifierValeur(dValeur);
-					pVARListe[uiElement].ModifierMin(dValeur);
+					// Recherche de la valeur minimale de l'interval
+					pcTexte = FindDoubleInLine(',');
+					bSuccess = ToDouble(pcTexte, dValeur);
+					free(pcTexte);
+
+					if(bSuccess)
+					{
+						pVARListe[uiElement].ModifierValeur(dValeur);
+						pVARListe[uiElement].ModifierMin(dValeur);
+					}
+					else
+					{
+						// Exception : Format non respecté
+					}
+
+					// Recherche de la valeur maximal de l'interval
+					pcTexte = FindDoubleInLine(']');
+					bSuccess = ToDouble(pcTexte, dValeur);
+					free(pcTexte);
+
+					if(bSuccess && dValeur >= pVARListe[uiElement].LireMin())
+					{
+						pVARListe[uiElement].ModifierMax(dValeur);
+						pVARListe[uiElement].ModifierVariable = (dValeur > pVARListe[uiElement].LireMin());
+					}
+					else
+					{
+						// Exception : Format non respecté
+					}
+
+					uiVariables++; // Un élément variable a été détecté dans le format correct	
 				}
 				else
 				{
-					// Exception : Format non respecté
+					// Exception : Format non respecté	
 				}
-				
-				// Recherche de la valeur maximal de l'interval
-				pcTexte = FindDoubleInLine(']');
-				bSuccess = ToDouble(pcTexte, dValeur);
-				free(pcTexte);
-				
-				if(bSuccess && dValeur >= pVARListe[uiElement].LireMin())
-				{
-					pVARListe[uiElement].ModifierMax(dValeur);
-					pVARListe[uiElement].ModifierVariable = (dValeur > pVARListe[uiElement].LireMin());
-				}
-				else
-				{
-					// Exception : Format non respecté
-				}
-				
-				uiVariables++; // Un élément variable a été détecté dans le format correct	
+
+				FindWordInFileLine("", ','); // On passe à l'élément suivant dans le fichier
+
 			}
 			else
 			{
-				// Exception : Format non respecté	
+				pVARListe[uiElement].ModifierValeur(dValeur);
+				pVARListe[uiElement].ModifierMin(dValeur);
+				pVARListe[uiElement].ModifierMax(dValeur);
+				pVarListe[uiElement].ModifierVariable(false);
 			}
-			
-			FindWordInFileLine("", ','); // On passe à l'élément suivant dans le fichier
-			
+
+			uiElement++; // Un élément a été créé
+
+		}
+
+		
+		// Création d'un parametre de Denavit après lecture de 4 variables sur la ligne
+		if(uiElement > 0 && (uiElement % 4) == 0)
+		{
+			uiNbParametres++;
+			if(pDENParametres == nullptr)
+			{
+				pDENParametres = (DenavitParameter*)malloc(sizeof(DenavitParameter)*1);
+			}
+			else
+			{
+				// Reallocation
+				DenavitParameter* temp = (DenavitParameter*)malloc(sizeof(DenavitParameter)*uiNbParametres);
+				for(uiBoucle = 0; uiBoucle < uiNbParametres-1; uiBoucle++)
+				{
+					temp[uiBoucle] = pDENParametres[uiBoucle];
+				}
+				free(pDENParametres);
+				pDENParametres = temp;
+			}
+
+			pDENParametres[uiNbParametres-1] = new DenavitParameter();
+
+			for(uiBoucle = 0; uiBoucle < 4; uiBoucle++)
+			{
+				pDENParametres[uiNbParametres-1].SetVariable(uiBoucle, pVARListe[uiElement-4+uiBoucle]);
+			}
+
+			NextLine();  // Passage à la ligne suivante
 		}
 		else
 		{
-			pVARListe[uiElement].ModifierValeur(dValeur);
-			pVARListe[uiElement].ModifierMin(dValeur);
-			pVARListe[uiElement].ModifierMax(dValeur);
-			pVarListe[uiElement].ModifierVariable(false);
+			// Exception : Il n'y a pas exactement 4 variables par ligne dans le fichier	
 		}
-		
-		uiElement++; // Un élément a été créé
-		
 	}
 	
 	
+	
+	// Enregistrement des éléments variables
 	uiNbElements = uiElement;
 	uiNbVariables = uiVariables;
 	
